86d65df33ac6be3f7a291cf0f7afe4b13f5368f5
Giorgi <Giorgi@LAPTOP-JPK2IU63>
2024-02-05 23:03:29 +0500

    Add project files.

---
+++ ConsoleApp8/clock/Clock.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Clock
+    {
+        int second;
+        int minute;
+        int hour;
+        public int Hour
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                if (value < 24 && value >= 0)
+                {
+                    hour = value;
+                }
+                else
+                {
+                    Console.WriteLine("You Can't Declare Hours");
+                }
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 60 && value >= 0)
+                {
+                    minute = value;
+                }
+                else
+                {
+                    Console.WriteLine("You Can't Declare Minutes");
+                }
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 60 && value >= 0)
+                {
+                    second = value;
+                }
+                else
+                {
+                    Console.WriteLine("You Can't Declare Seconds");
+                }
+            }
+        }
+        public void currentTime()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (hour < 10)
+            {
+                sb.Append("0");
+                sb.Append(hour);
+            }
+            else
+            {
+                sb.Append(hour);
+            }
+            sb.Append(":");
+
+
+            if (minute < 10)
+            {
+                sb.Append("0");
+                sb.Append(minute);
+            }
+            else
+            {
+                sb.Append(minute);
+            }
+            sb.Append(":");
+            if (second < 10)
+            {
+                sb.Append("0");
+                sb.Append(second);
+            }
+            else
+            {
+                sb.Append(second);
+            }
+
+            Console.WriteLine(" Time:"+sb.ToString());
+        }
+
+
+        public void addSeconds(int sec)
+        {
+            for (int i = 0; i < sec; i++)
+            {
+                if (second == 59)
+                {
+                    second = 0;
+                    addMinute();
+                }
+                else
+                {
+                    second++;
+                }
+            }
+        }
+        public void addMinute()
+        {
+            if (minute == 59)
+            {
+                minute = 0;
+                addHour();
+            }
+            else
+            {
+                minute++;
+            }
+
+        }
+        public void addHour()
+        {
+            if (hour==23)
+            {
+                hour = 0;
+            }
+            else
+            {
+                hour++;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp8/clock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            
+            Console.WriteLine("Enter Hours:");
+            clock.Hour=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Minutes:");
+            clock.Minute = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter Seconds:");
+            clock.Second = int.Parse(Console.ReadLine());
+
+
+            
+           
+            Console.Write("Entered");
+            clock.currentTime();
+
+            clock.addSeconds(4);
+            Console.Write("New");
+            clock.currentTime();
+
+            clock.addSeconds(7);
+            Console.Write("New(2)");
+            clock.currentTime();
+
+
+            Console.ReadKey();
+        }
+    }
+}
